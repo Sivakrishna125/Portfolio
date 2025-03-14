7c2dd02dea096119c29b64b72c03b998e6b3aa8b
Sivakrishna125 <sivakrishna.kolagani@sasi.ac.in>
2025-03-14 13:33:42 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import Home from './Home';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:5000')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the data', error);
-      });
-  }, []);
-
+ 
   return (
     <div>
-      {data ? (
-        <div>
-          <h1>{data.title}</h1>
-          <p>{data.description}</p>
-          <p>{data.details}</p>
-        </div>
-      ) : (
-        <div>
-          
-        </div>
-      )}
+      
       <Home/>
     </div>
   );
